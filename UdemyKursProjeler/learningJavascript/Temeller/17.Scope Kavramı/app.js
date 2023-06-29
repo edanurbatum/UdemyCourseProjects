1f6b228b09cbd314849b8e09eec26de70dc2464a
Edanur Batum <61375345+edanurbatum@users.noreply.github.com>
2023-06-29 19:58:13 +0300

    Kurs
    Kurs

---
+++ UdemyKursProjeler/learningJavascript/Temeller/17.Scope Kavramı/app.js
@@ -0,0 +1,31 @@
+//Scope Kavramý
+
+var value1 = 10;
+let value2 = 20;
+const value3 = 30;
+
+//function Function1() {
+//    var value1 = 40;
+//    let value2 = 50;
+//    const value3 = 60;
+
+//    console.log(value1, value2, value3);
+//}
+
+//Function1();
+
+if (true) {
+    var value1 = 40;
+    let value2 = 50;
+    const value3 = 60;
+
+    //console.log(a, b, c);
+}
+console.log(value1, value2, value3);
+
+//b ve c undefined olur, let ve const blok dýþýnda var olmaz
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+//console.log(value1, value2, value3);
